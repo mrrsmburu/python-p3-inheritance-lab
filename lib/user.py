743c7249e976d1892549d2b524f613a10552263e
mrrsmburu <mrrsmburu@gmail.com>
2024-05-27 16:34:44 +0300

    completed assignment

--- lib/user.py
+++ lib/user.py
@@ -5,12 +5,3 @@ class User:
         self.first_name = first_name
         self.last_name = last_name
 
-
-if __name__ == "__main__":
-    
-    user_instance = User(first_name="John", last_name="Doe")
-
-    
-    print(f"First Name: {user_instance.first_name}")
-    print(f"Last Name: {user_instance.last_name}")
-    pass
